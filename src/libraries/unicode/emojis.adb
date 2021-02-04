0197e3b3223f9dad90fa68e8fb732ef488c6576a
cnngimenez <christian.gimenez@fi.uncoma.edu.ar>
2021-02-04 11:20:04 -0300

    Fixed style.

--- src/libraries/unicode/emojis.adb
+++ src/libraries/unicode/emojis.adb
@@ -1,49 +1,42 @@
--- emojis.adb --- 
+--  emojis.adb ---
 
--- Copyright 2019 cnngimenez
+--  Copyright 2019 cnngimenez
 --
--- Author: cnngimenez
+--  Author: cnngimenez
 
--- This program is free software: you can redistribute it and/or modify
--- it under the terms of the GNU General Public License as published by
--- the Free Software Foundation, either version 3 of the License, or
--- (at your option) any later version.
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
 
--- This program is distributed in the hope that it will be useful,
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
--- GNU General Public License for more details.
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
 
--- You should have received a copy of the GNU General Public License
--- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 -------------------------------------------------------------------------
 
-with Ada.Wide_Wide_Text_Io;
-use Ada.Wide_Wide_Text_Io;
+with Ada.Wide_Wide_Text_IO;
+use Ada.Wide_Wide_Text_IO;
 
 package body Emojis is
-    function Get_Skin (Skin : Skin_Type) return Wide_Wide_Character is
-    begin
-        return Wide_Wide_Character'Val 
-          (Skin_Type'Pos (Skin) + 16#1F3FB#);
-    end Get_Skin;
-    
-    function Get_Skin (Emoji : Wide_Wide_Character;
-                       Skin : Skin_Type)
-                      return Wide_Wide_String
+
+    function Get_Emoji_Variation (Char : Wide_Wide_Character)
+                                 return Wide_Wide_String
     is
     begin
-        return Emoji & Wide_Wide_Character'Val
-          (Skin_Type'Pos (Skin) + 16#1f3fb#);
-    end Get_Skin;
-    
+        return Char & Variation_Selector;
+    end Get_Emoji_Variation;
+
     function Get_Hair (Hair : Hair_Type) return Wide_Wide_Character is
     begin
         return Wide_Wide_Character'Val
           (Hair_Type'Pos (Hair) + 16#1F9B0#);
     end Get_Hair;
-    
+
     function Get_Hair (Emoji : Wide_Wide_Character;
                        Hair : Hair_Type)
                       return Wide_Wide_String
@@ -52,24 +45,32 @@ package body Emojis is
         return Emoji & Wide_Wide_Character'Val
           (Hair_Type'Pos (Hair) + 16#1f9b0#);
     end Get_Hair;
-    
-    function Get_Keycap (Char : Wide_Wide_Character) 
+
+    function Get_Keycap (Char : Wide_Wide_Character)
                         return Wide_Wide_String
     is
     begin
         return Char & Combining_Enclosing_Keycap;
     end Get_Keycap;
-    
-    function Get_Emoji_Variation (Char : Wide_Wide_Character)
-                                 return Wide_Wide_String
+
+    function Get_Skin (Skin : Skin_Type) return Wide_Wide_Character is
+    begin
+        return Wide_Wide_Character'Val
+          (Skin_Type'Pos (Skin) + 16#1F3FB#);
+    end Get_Skin;
+
+    function Get_Skin (Emoji : Wide_Wide_Character;
+                       Skin : Skin_Type)
+                      return Wide_Wide_String
     is
     begin
-        return Char & Variation_Selector;
-    end Get_Emoji_Variation;
-    
-    function Join_With_Zwj (Characters : Wide_Wide_String) 
+        return Emoji & Wide_Wide_Character'Val
+          (Skin_Type'Pos (Skin) + 16#1f3fb#);
+    end Get_Skin;
+
+    function Join_With_Zwj (Characters : Wide_Wide_String)
                            return Unbounded_Wide_Wide_String
-    is 
+    is
         Str_Output : Unbounded_Wide_Wide_String;
         Char : Wide_Wide_Character;
     begin
@@ -78,63 +79,63 @@ package body Emojis is
             Append (Str_Output, Char);
             Append (Str_Output, Zero_Width_Joiner);
         end loop;
-        
+
         return Str_Output;
     end Join_With_Zwj;
-    
-    function Join_With_Zwj (Characters : Wide_Wide_String) 
+
+    function Join_With_Zwj (Characters : Wide_Wide_String)
                            return Wide_Wide_String
-    is 
+    is
         Unbounded_Str : Unbounded_Wide_Wide_String;
     begin
         Unbounded_Str := Join_With_Zwj (Characters);
-        
-        return To_Wide_Wide_String (Unbounded_Str);  
+
+        return To_Wide_Wide_String (Unbounded_Str);
     end Join_With_Zwj;
 
-    procedure Put_Skin (Skin : Skin_Type) is
-    begin
-        Put (Get_Skin (Skin));        
-    end Put_Skin;
-    
-    procedure Put_Skin (Emoji : Wide_Wide_Character; Skin : Skin_Type) is
-    begin
-        Put (Get_Skin (Emoji, Skin));        
-    end Put_Skin;    
-    
-    procedure Put_Hair (Hair : Hair_Type) is
-    begin
-        Put (Get_Hair (Hair));
-    end Put_Hair;
-    
-    procedure Put_Hair (Emoji : Wide_Wide_Character; Hair : Hair_Type) is
-    begin
-        Put (Get_Hair (Emoji, Hair));
-    end Put_Hair;
-    
-    procedure Put_Person is
-    begin
-        Put (Wide_Wide_Character'Val (16#1F9D1#));
-    end Put_Person;
-    
     procedure Put_Emoji (Number : Positive) is
     begin
         Put (Wide_Wide_Character'Val (Number));
     end Put_Emoji;
-    
-    procedure Put_Zwj is 
+
+    procedure Put_Hair (Hair : Hair_Type) is
     begin
-        Put (Zero_Width_Joiner);
-    end Put_Zwj;
-    
-    procedure Put_Variation is 
+        Put (Get_Hair (Hair));
+    end Put_Hair;
+
+    procedure Put_Hair (Emoji : Wide_Wide_Character; Hair : Hair_Type) is
     begin
-        Put (Variation_Selector);
-    end Put_Variation;
+        Put (Get_Hair (Emoji, Hair));
+    end Put_Hair;
 
     procedure Put_Keycap is
     begin
         Put (Combining_Enclosing_Keycap);
     end Put_Keycap;
+
+    procedure Put_Person is
+    begin
+        Put (Wide_Wide_Character'Val (16#1F9D1#));
+    end Put_Person;
+
+    procedure Put_Skin (Skin : Skin_Type) is
+    begin
+        Put (Get_Skin (Skin));
+    end Put_Skin;
+
+    procedure Put_Skin (Emoji : Wide_Wide_Character; Skin : Skin_Type) is
+    begin
+        Put (Get_Skin (Emoji, Skin));
+    end Put_Skin;
+
+    procedure Put_Variation is
+    begin
+        Put (Variation_Selector);
+    end Put_Variation;
+
+    procedure Put_Zwj is
+    begin
+        Put (Zero_Width_Joiner);
+    end Put_Zwj;
 
 end Emojis;
--- src/libraries/unicode/emojis.ads
+++ src/libraries/unicode/emojis.ads
@@ -1,21 +1,21 @@
--- emojis.ads --- 
+--  emojis.ads ---
 
--- Copyright 2019 cnngimenez
+--  Copyright 2019 cnngimenez
 --
--- Author: cnngimenez
+--  Author: cnngimenez
 
--- This program is free software: you can redistribute it and/or modify
--- it under the terms of the GNU General Public License as published by
--- the Free Software Foundation, either version 3 of the License, or
--- (at your option) any later version.
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
 
--- This program is distributed in the hope that it will be useful,
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
--- GNU General Public License for more details.
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
 
--- You should have received a copy of the GNU General Public License
--- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 -------------------------------------------------------------------------
 
@@ -23,74 +23,72 @@ with Ada.Strings.Wide_Wide_Unbounded;
 use Ada.Strings.Wide_Wide_Unbounded;
 
 package Emojis is
-    
-    -- Emoji components
-    -- Regional_Indicator is ...
-    
-    --  Some emojis like faces, hands and persons allows to change the 
+
+    --  Emoji components
+    --  Regional_Indicator is ...
+
+    --  Some emojis like faces, hands and persons allows to change the
     --  skin tone. Use this and Get_Skin function to add one of the possible
     --  skins.
     type Skin_Type is (Light, Medium_Light, Medium, Medium_Dark, Dark);
-    
+
     --  Same as Skin_Type. Change the hear style with Get_Hair function.
     type Hair_Type is (Red_Hair, Curly_Hair, Bald, White_Hair);
-    
+
     --  Some fonts and services allows to use two or more emojis to create one.
     --  For example, joining a speech bubble + zero_width_joiner + eye creates
     --  a speech bubble with an eye inside.
-    Zero_Width_Joiner : constant Wide_Wide_Character := 
-      Wide_Wide_Character'Val(16#200D#);
-    
+    Zero_Width_Joiner : constant Wide_Wide_Character :=
+      Wide_Wide_Character'Val (16#200D#);
+
     --  Shows the simple character that is before as a keyboard key.
-    Combining_Enclosing_Keycap : constant Wide_Wide_Character := 
-      Wide_Wide_Character'Val(16#20E3#);
-    
+    Combining_Enclosing_Keycap : constant Wide_Wide_Character :=
+      Wide_Wide_Character'Val (16#20E3#);
+
     --  Variation selector allows to choose the imaged emoji instead the simple
     --  monospaced font.
-    Variation_Selector : constant Wide_Wide_Character := 
-      Wide_Wide_Character'Val(16#FE0F#);
-    
+    Variation_Selector : constant Wide_Wide_Character :=
+      Wide_Wide_Character'Val (16#FE0F#);
+
     function Get_Skin (Skin : Skin_Type) return Wide_Wide_Character;
     function Get_Skin (Emoji : Wide_Wide_Character;
                        Skin : Skin_Type) return Wide_Wide_String;
     function Get_Hair (Hair : Hair_Type) return Wide_Wide_Character;
     function Get_Hair (Emoji : Wide_Wide_Character;
                        Hair : Hair_Type) return Wide_Wide_String;
-    
-    function Get_Keycap (Char : Wide_Wide_Character) 
+
+    function Get_Keycap (Char : Wide_Wide_Character)
                         return Wide_Wide_String;
     function Get_Emoji_Variation (Char : Wide_Wide_Character)
                                  return Wide_Wide_String;
-    
-    --  Add a Zero Width Joiner (ZWJ) character between the input string's 
+
+    --  Add a Zero Width Joiner (ZWJ) character between the input string's
     --  characters.
-    function Join_With_Zwj (Characters : Wide_Wide_String) 
+    function Join_With_Zwj (Characters : Wide_Wide_String)
                            return Unbounded_Wide_Wide_String;
-                            
+    function Join_With_Zwj (Characters : Wide_Wide_String)
+                           return Wide_Wide_String;
     --
     --  Printing functions.
     --
-    
+
     procedure Put_Skin (Skin : Skin_Type);
     procedure Put_Skin (Emoji : Wide_Wide_Character; Skin : Skin_Type);
     procedure Put_Hair (Hair : Hair_Type);
     procedure Put_Hair (Emoji : Wide_Wide_Character; Hair : Hair_Type);
-    
+
     --  Put the given emoji character.
     procedure Put_Emoji (Number : Positive);
-    
+
     --  Put the Zero Width Joiner to standard IO
     procedure Put_Zwj;
-    
+
     --  Put the Variation Selector to standard IO
     procedure Put_Variation;
 
     --  Put to standard IO the Combining Enclosing Keycap
     procedure Put_Keycap;
-    
-    
+
     procedure Put_Person;
-    
-    
-    
+
 end Emojis;
