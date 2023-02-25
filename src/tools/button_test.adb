402ca2203b3537bdf656f30e20e83a4cad36e5c9
cnngimenez <cnngimenez@disroot.org>
2023-02-24 22:54:53 -0300

    Button_Test: Mouse move handler and button box.

--- src/tools/button_test.adb
+++ src/tools/button_test.adb
@@ -33,26 +33,47 @@ use Widgets.Labels;
 procedure Button_Test is
 
     procedure Hello_Click (Mouse_Event : Mouse.Mouse_Event_Type);
+    procedure Mouse_Move (Mouse_Event : Mouse.Mouse_Event_Type);
 
     Tui : Ada_Tui_Type;
     Hello_Label : aliased Label_Type;
     Hello_Button : aliased Button_Type;
+    Quit : Boolean := False;
 
     procedure Hello_Click (Mouse_Event : Mouse.Mouse_Event_Type) is
         pragma Unreferenced (Mouse_Event);
     begin
-        --  Hello_Label.Set_Text ("The button was Clicked!");
+        Hello_Label.Set_Text ("The button was Clicked!");
         Put_Line ("Trying a simple put_line");
+        Quit := True;
     end Hello_Click;
+
+    procedure Mouse_Move (Mouse_Event : Mouse.Mouse_Event_Type) is
+        pragma Unreferenced (Mouse_Event);
+    begin
+        Hello_Button.Set_Text ("Press Me!!!  ");
+    end Mouse_Move;
 
     Click_Handler : constant Widgets.Mouse_Handler :=
         Hello_Click'Unrestricted_Access;
+    Move_Handler : constant Widgets.Mouse_Handler :=
+        Mouse_Move'Unrestricted_Access;
 
     --  W : Widgets.Widget_Access_Type;
+
+    Config : Widgets.Widget_Config_Type :=
+        Widgets.Default_Widget_Config;
+
 begin
-    Hello_Label.Initialize ("Welcome!", 2,  2, 10, 3);
+    Hello_Label.Initialize ("Welcome!", 2, 2, 10, 3);
     Hello_Button.Initialize ("Press button!", 10, 10);
+    Hello_Button.Set_Width (17);
+
     Hello_Button.Set_Mouse_Click_Handler (Click_Handler);
+    Hello_Button.Set_Mouse_Move_Handler (Move_Handler);
+
+    Config.Draw_Border := Widgets.Border_Simple;
+    Hello_Button.Set_Config (Config);
 
     Tui.Add_Widget (Hello_Label'Unrestricted_Access);
     Tui.Add_Widget (Hello_Button'Unrestricted_Access);
@@ -62,7 +83,7 @@ begin
         Tui.Draw;
         Tui.Event_Loop;
 
-        exit when Tui.Last_Key_Pressed = 'q';
+        exit when Tui.Last_Key_Pressed = 'q' or else Quit;
     end loop;
 
 end Button_Test;
