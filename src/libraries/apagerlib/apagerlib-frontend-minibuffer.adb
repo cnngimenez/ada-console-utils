b793cc5fb068573a8c39f1a8066a2407f2be7728
cnngimenez <cnngimenez@disroot.org>
2024-08-18 18:32:01 -0300

    Minibuffer type.

---
+++ src/libraries/apagerlib/apagerlib-frontend-minibuffer.adb
@@ -0,0 +1,47 @@
+--  apagerlib-frontend-minibuffer.adb ---
+
+--  Copyright 2024 cnngimenez
+--
+--  Author: cnngimenez
+
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+-------------------------------------------------------------------------
+
+with Ada.Text_IO;
+use Ada.Text_IO;
+with Ada.Strings.Fixed;
+use Ada.Strings.Fixed;
+
+with Console.CSI_Codes;
+use Console.CSI_Codes;
+
+package body Apagerlib.Frontend.Minibuffer is
+
+    function Minibuffer_String (Minibuffer : Minibuffer_Type)
+        return Unbounded_String
+    is ((if Minibuffer.Meta_X then "M-x: " else "")
+        & Minibuffer.Message);
+
+    procedure Put_Minibuffer (Minibuffer : Minibuffer_Type) is
+        Str : Unbounded_String;
+    begin
+        Cursor_Position (Minibuffer.Position_Line, Minibuffer.Position_Column);
+        Str := Minibuffer_String (Minibuffer);
+
+        Put_Line (To_String (Str)
+            & (Minibuffer.Width - Length (Str)) * ' ');
+    end Put_Minibuffer;
+
+end Apagerlib.Frontend.Minibuffer;
---
+++ src/libraries/apagerlib/apagerlib-frontend-minibuffer.ads
@@ -0,0 +1,50 @@
+--  apagerlib-frontend-minibuffer.ads ---
+
+--  Copyright 2024 cnngimenez
+--
+--  Author: cnngimenez
+
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+-------------------------------------------------------------------------
+
+with Ada.Strings.Unbounded;
+use Ada.Strings.Unbounded;
+
+package Apagerlib.Frontend.Minibuffer is
+
+    type Minibuffer_Type is tagged record
+        Position_Column, Position_Line : Positive;
+        --  Where to start drawing the minibuffer.
+        Width, Height : Positive;
+        --  Width and height in characters (columns) and lines.
+        Meta_X : Boolean;
+        --  Is M-x pressed?
+        Message : Unbounded_String;
+    end record;
+
+    function Minibuffer_String (Minibuffer : Minibuffer_Type)
+        return Unbounded_String;
+
+    procedure Put_Minibuffer (Minibuffer : Minibuffer_Type);
+
+    Default_Minibuffer : constant Minibuffer_Type := (
+        Position_Column => 1,
+        Position_Line => 25,
+        Width => 25,
+        Height => 25,
+        Meta_X => False,
+        Message => To_Unbounded_String (""));
+
+end Apagerlib.Frontend.Minibuffer;
